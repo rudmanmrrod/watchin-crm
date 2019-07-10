f72831b95bba0eb580cdba72c56af7821c1817a9
Rudmanmrrod <rudmanmrrod@gmail.com>
2019-07-10 09:53:32 -0400

    Added navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,32 @@
 <template>
   <v-app>
-    <v-toolbar app>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      clipped
+      app
+    >
+      <v-list class="pt-0" dense>
+
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+          @click=""
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <router-link :to="item.url">
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </router-link>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span>Vuetify</span>
         <span class="font-weight-light">Watchin SGE</span>
@@ -14,8 +40,18 @@
 </template>
 
 <script>
+import "./App.css";
+
 export default {
   name: 'App',
+  data(){
+    return {
+      drawer: false,
+      items: [
+        {icon:'home', title: 'home', url: "/home"}
+      ]
+    }
+  },
   mounted(){
     if(this.$store.state.users.user){
       this.$router.push('/')
